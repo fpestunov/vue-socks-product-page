fe6f1cb16eda6cbeae6d1e078b5f392c7ad47505
Fedor Pestunov <fpestunov@gmail.com>
2019-01-28 17:56:49 +0400

    add atribute binding

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ var app = new Vue({
   el: '#app',
   data: {
     product: 'Socks',
-    description: 'A pair of warm, fuzzy socks'
+    description: 'A pair of warm, fuzzy socks',
+    altText: "A pair of socks",
+    image: "./images/socks-gr.jpg",
+    link: "https://yandex.ru/search/?text=socks"
   }
 })
