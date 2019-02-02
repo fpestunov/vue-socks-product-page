34e490d4f2487de3d265bdbc695a623f59834b6a
fpestunov <fpestunov@gmail.com>
2019-02-02 18:05:43 +0400

    add conditional rendering

---
+++ lesson03/main.js
@@ -0,0 +1,15 @@
+let product = 'socks';
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    product: 'Socks',
+    description: 'A pair of warm, fuzzy socks',
+    altText: "A pair of socks",
+    image: "./images/socks-gr.jpg",
+    link: "https://yandex.ru/search/?text=socks",
+    inStock: true,
+    inventory: 100,
+    onSale: true
+  }
+})
